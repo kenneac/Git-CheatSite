8a12f068b16e98f1287cd622ad3570ae9a315548
kenneac <iamkenneac@gmail.com>
2026-06-08 10:30:05 +0100

    feat: code cleanup

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const navItems = document.querySelectorAll("body>nav li");
 navItems.forEach(item => {
   item.addEventListener("click", () => {
     navItems.forEach(i => i.classList.remove("active"));
-    
+
     item.classList.add("active");
   });
 });
@@ -39,43 +39,3 @@ document
 document
   .getElementById("btn-viewExtras")
   .addEventListener("click", () => switchView("arrangeViewExtras"));
-
-//----------------- IMPLEMENTING media = "..." FOR STYLE SWITCHING -----------------
-// ------ TO USE THIS: REMOVE THE CLASS NAMES ABOVE FROM <BODY> --------------------
-// const sheets = {
-//   viewAll: document.getElementById("cssViewAll"),
-//   extras: document.getElementById("cssViewExtras"),
-//   main: document.getElementById("cssViewMainContent"),
-//   workflow: document.getElementById("cssViewWorkFlow"),
-// };
-
-// // function unapplyAllMediaExcept(activeSheet) {
-// //   Object.values(sheets).forEach((sheet) => {
-// //     sheet.media = sheet === activeSheet ? "all" : "none";
-// //   });
-// // }
-
-// // function onViewAll() {
-// //   unapplyAllMediaExcept(sheets.viewAll);
-// // }
-// // function onViewExtras() {
-// //   unapplyAllMediaExcept(sheets.extras);
-// // }
-// // function onViewMainContent() {
-// //   unapplyAllMediaExcept(sheets.main);
-// // }
-// // function onViewWorkFlow() {
-// //   unapplyAllMediaExcept(sheets.workflow);
-// // }
-
-// // bind buttons to the functions
-// document.getElementById("btn-viewAll").addEventListener("click", onViewAll);
-// document
-//   .getElementById("btn-viewExtras")
-//   .addEventListener("click", onViewExtras);
-// document
-//   .getElementById("btn-viewMainContent")
-//   .addEventListener("click", onViewMainContent);
-// document
-//   .getElementById("btn-viewWorkFlow")
-//   .addEventListener("click", onViewWorkFlow);
